e73f54ad8e5b85090211e88e5380b5c459afc088
Nishant <nishchaynish8@gmail.com>
2025-07-27 21:08:10 +0530

    done

--- Frontend/src/components/addUser/Add.jsx
+++ Frontend/src/components/addUser/Add.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import "./add.css";
+import { Link } from "react-router-dom";
 
 const Add = () => {
   return (
-    <div>Add</div>
-  )
-}
+    <div className="addUser">
+      <Link to={"/"}>Back</Link>
+      <h3>Add new user</h3>
 
-export default Add
+      <form className="addUserForm ">
+        <div className="inputGroup">
+          <label htmlFor="firstName">First name</label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            autoComplete="off"
+            placeholder="First Name"
+          />
+        </div>
+
+        <div className="inputGroup">
+          <label htmlFor="lastName">Last name</label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            autoComplete="off"
+            placeholder="Last Name"
+          />
+        </div>
+
+        <div className="inputGroup">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            autoComplete="off"
+            placeholder="Email"
+          />
+        </div>
+
+        <div className="inputGroup">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            autoComplete="off"
+            placeholder="Password"
+          />
+        </div>
+
+        <div className="inputGroup">
+                <button type="submit">ADD USER</button>
+            </div>
+      </form>
+    </div>
+  );
+};
+
+export default Add;
--- Frontend/src/components/getUser/User.jsx
+++ Frontend/src/components/getUser/User.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import './user.css'
 
 const User = () => {
   return (
-    <div>User</div>
+    <div className="userTable">
+      <Link to={"/add"} className='addButton'> Add Users</Link>
+      <table border={1} cellPadding={10} cellSpacing={0}>
+        <thead>
+          <tr>
+            <th>S.NO.</th>
+            <th>User Name</th>
+            <th>User Email</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <td>1.</td>
+          <td>Nishant Chauhan</td>
+          <td>Nish@gmail.com</td>
+          <td className='actionButtons'>
+            <button><i className="fa-solid fa-trash"></i></button>
+            <Link to={"/edit"}><i className="fa-solid fa-pen-to-square"></i></Link>
+          </td>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
